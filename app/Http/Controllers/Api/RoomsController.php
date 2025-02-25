edeb0cf665df73a5768f90c70eac01f65f0cc6f5
iargo anjos rocha <yargo.ar@gmail.com>
2025-02-25 17:54:45 -0500

    CORS file

--- app/Http/Controllers/Api/RoomsController.php
+++ app/Http/Controllers/Api/RoomsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Events\RoomUpdated;
 use App\Http\Controllers\Controller;
+use App\Managers\GameRoomManager;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -50,76 +51,6 @@ class RoomsController extends Controller
 
     public function getRooms(Request $request)
     {
-        return response()->json(
-            json_decode('{
-    "rooms": [
-        {
-            "number": "1",
-            "private":"false",
-            "players": {
-                "home": {
-                    "id": "1",
-                    "name": "User 1",
-                    "skin": "0",
-                    "score": "1"
-                },
-                "visitant": {
-                    "id": "2",
-                    "name": "User 2",
-                    "skin": "0",
-                    "score": "0"
-                }
-            },
-            "audience": [
-                {
-                    "id": "31",
-                    "name": "User 3",
-                    "skin": "0"
-                },
-                {
-                    "id": "4",
-                    "name": "User 4",
-                    "skin": "0"
-                }
-            ],
-            "game_choosen": [
-                "tic-tac-toe"
-            ]
-        },
-        {
-            "number": "2",
-            "user1": "",
-            "user2": "",
-            "audience": [
-                "",
-                ""
-            ],
-            "score": [
-                "1",
-                "0"
-            ],
-            "game_choosen": [
-                "tic-tac-toe"
-            ]
-        },
-        {
-            "number": "3",
-            "user1": "",
-            "user2": "",
-            "audience": [
-                "",
-                ""
-            ],
-            "score": [
-                "1",
-                "0"
-            ],
-            "game_choosen": [
-                "tic-tac-toe"
-            ]
-        }
-    ]
-}')
-        );
+        return GameRoomManager::getRooms();
     }
 }
